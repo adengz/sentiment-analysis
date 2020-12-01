c994f194cf02507337af0ec99f3c855ebcf910fa
adengz <adengz@gmail.com>
2020-12-01 15:35:08 +0800

    feat: Add dot product self attention and residual connection option

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -56,8 +58,8 @@ class WordAveragingModel(nn.Module):
 
 class AttentionWeightedWordAveragingModel(nn.Module):
 
-    def __init__(self, vocab_size: int, embed_dim: int, attention: nn.Module,
-                 embed_dropout: float = 0.5, pad_idx: int = Vocabulary.pad_idx):
+    def __init__(self, vocab_size: int, embed_dim: int, attention: Callable[[torch.Tensor], torch.Tensor],
+                 res_conn: bool = False, embed_dropout: float = 0.5, pad_idx: int = Vocabulary.pad_idx):
         """
         Adding attention weights on top of word averaging model.
 
@@ -65,6 +67,8 @@ class AttentionWeightedWordAveragingModel(nn.Module):
             vocab_size: Vocabulary size.
             embed_dim: Word embedding dimension.
             attention: Attention calculator.
+            res_conn: Whether apply residual connection to weighted
+                hidden state with average embedding. Default: False
             embed_dropout: Dropout applied on word embedding.
                 Default: 0.5
             pad_idx: Index of padding token in vocabulary.
@@ -75,6 +79,7 @@ class AttentionWeightedWordAveragingModel(nn.Module):
         self.embed_dropout = nn.Dropout(embed_dropout)
         self.attention = attention
         self.fc = nn.Linear(embed_dim, 1)
+        self.res_conn = res_conn
 
         init_range = 0.5 / embed_dim
         self.embedding.weight.data.uniform_(-init_range, init_range)
@@ -93,6 +98,8 @@ class AttentionWeightedWordAveragingModel(nn.Module):
         embedded = self.embed_dropout(self.embedding(inp))  # seq_len, batch_size, embed_dim
         attention = self.attention(embedded).unsqueeze(2)  # seq_len, batch_size, 1
         hidden = torch.mul(embedded, attention).sum(0)  # batch_size, embed_dim
+        if self.res_conn:
+            hidden += embedded.mean(0)
         logit = self.fc(hidden).squeeze()  # batch_size
         return logit
 
@@ -137,3 +144,20 @@ class UAttention(nn.Module):
             *
         """
         return F.cosine_similarity(embedded, self.u, dim=-1)
+
+
+def dot_product_self_attention(embedded: torch.Tensor) -> torch.Tensor:
+    """
+    Self attention computed from dot product with other embedded word
+    vectors in the same sequence.
+
+    Args:
+        embedded: seq_len, batch_size, embed_dim
+
+    Returns:
+        seq_len, batch_size
+    """
+    permuted = embedded.permute(1, 0, 2)  # batch_size, seq_len. embed_dim
+    summed_dot_prod = torch.bmm(permuted, permuted.transpose(1, 2)).sum(2)  # batch_size, seq_len
+    attention = F.softmax(summed_dot_prod.transpose(0, 1), dim=0)  # seq_len, batch_size
+    return attention
