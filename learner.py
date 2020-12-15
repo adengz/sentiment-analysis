0a41a6d0a0211260daa74af5da956765f05e06be
adengz <adengz@gmail.com>
2020-12-15 15:36:41 +0800

    refactor: Get dataloader inside learner

--- learner.py
+++ learner.py
@@ -3,28 +3,33 @@ import time
 
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
 from torch.optim import Optimizer
+
+from data import SentiDataset, get_dataloader
 
 
 class SentimentLearner:
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    def __init__(self, model: nn.Module, train_loader: DataLoader, valid_loader: DataLoader,
-                 optim_cls: Callable[..., Optimizer], lr: float):
+    def __init__(self, model: nn.Module, train_set: SentiDataset, valid_set: SentiDataset, test_set: SentiDataset,
+                 batch_size: int, optim_cls: Callable[..., Optimizer], lr: float):
         """
+        Learner for training binary sentiment analysis models.
 
         Args:
             model: Model.
-            train_loader: DataLoader for training dataset.
-            valid_loader: DataLoader for validation dataset.
+            train_set: Training dataset.
+            valid_set: Validation dataset.
+            test_set: Testing dataset.
+            batch_size: Batch size.
             optim_cls: Optimizer class.
             lr: Learning rate.
         """
         self.model = model.to(self.device)
-        self.train_loader = train_loader
-        self.valid_loader = valid_loader
+        self.train_loader = get_dataloader(train_set, batch_size)
+        self.valid_loader = get_dataloader(valid_set, batch_size)
+        self.test_loader = get_dataloader(test_set, batch_size)
         self.loss_fn = nn.BCEWithLogitsLoss()
         self.optimizer = optim_cls(self.model.parameters(), lr=lr)
 
@@ -51,20 +56,19 @@ class SentimentLearner:
         return loss, accuracy, len(targets)
 
     @torch.no_grad()
-    def evaluate(self, data_loader: DataLoader = None) -> Tuple[float, float]:
+    def evaluate(self, valid: bool = False) -> Tuple[float, float]:
         """
         Evaluates metrics with a given DataLoader or just valid_loader.
 
         Args:
-            data_loader: DataLoader, use valid_loader if not provided.
-                Default: None
+            valid: Use valid dataset (True) or test dataset (False).
+                Default: False
 
         Returns:
             Loss, accuracy
         """
         self.model.eval()
-        if data_loader is None:
-            data_loader = self.valid_loader
+        data_loader = self.valid_loader if valid else self.test_loader
 
         epoch_loss = epoch_acc = total_count = 0
         for batch in data_loader:
@@ -113,7 +117,7 @@ class SentimentLearner:
         for epoch in range(epochs):
             start = time.time()
             train_loss, train_acc = self._train_1_epoch()
-            valid_loss, valid_acc = self.evaluate()
+            valid_loss, valid_acc = self.evaluate(valid=True)
             end = time.time()
 
             print(f'Epoch : {epoch + 1:02}\tWall time : {end - start:.3f}s')
